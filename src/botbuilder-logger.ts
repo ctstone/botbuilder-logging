707f391b81d7e1ad92e3bb9f01e5012f3eac0d7f
Chris Stone <chstone@microsoft.com>
2017-04-11 17:29:30 -0400

    fix callbacks and configs

--- src/botbuilder-logger.ts
+++ src/botbuilder-logger.ts
@@ -27,25 +27,43 @@ export class BotLogger extends EventEmitter {
     private documentClient: DocumentClient,
     private options: BotLoggerOptions) {
       super();
-      const self = this;
       this.logger = new Logger()
-        .add(DocumentDbLogger as any, Object.assign({client: documentClient}, options))
+        .add(DocumentDbLogger as any, Object.assign({client: documentClient}, options.documents))
         .add(transports.Console, { level: 'error' });
       this.logger.transports.documentdb.on('media', (event: Media) => this.storeMedia(event));
     }
 
   register(bot: UniversalBot|UniversalCallBot): void {
     bot = bot as UniversalBot;
-    bot.on('error', (err) => this.logger.error(err && err.message ? err.message : 'Error', err));
-    bot.on('incoming', (event: IEvent) => this.logger.info(event.type, event));
-    bot.on('outgoing', (event: IEvent) => this.logger.info(event.type, event));
-    bot.on('routing', (session) => this.logger.info('routing', session));
+    bot.on('error', (err) => this.onBotError(err));
+    bot.on('incoming', (event: IEvent) => this.onBotEvent(event));
+    bot.on('outgoing', (event: IEvent) => this.onBotEvent(event));
+    bot.on('routing', (session) => this.onBotRouting(event));
     this.blobService.createContainerIfNotExists(this.options.blobs.container, (err) => {
-      if (err) {
-        this.emit('error', err);
+      this.onCallback(err);
+      if (!err) {
+        this.initialized = true;
       }
-      this.initialized = true;
     });
+  }
+
+  private onBotError(err: Error): void {
+    const message = err && err.message ? err.message : 'Error';
+    this.logger.error(message, err, (err) => this.onCallback);
+  }
+
+  private onBotEvent(event: IEvent): void {
+    this.logger.info(event.type, event, (err) => this.onCallback);
+  }
+
+  private onBotRouting(session: any): void {
+    this.logger.info('routing', session, (err) => this.onCallback);
+  }
+
+  private onCallback(err: Error) {
+    if (err) {
+      this.emit('error', err);
+    }
   }
 
   private storeMedia(event: Media): void {
@@ -57,10 +75,6 @@ export class BotLogger extends EventEmitter {
     const sas = this.blobService.generateSharedAccessSignature(this.options.blobs.container, name, this.policy);
     const url = this.blobService.getUrl(this.options.blobs.container, name, sas);
     event.id = url;
-    this.blobService.createBlockBlobFromText(this.options.blobs.container, name, event.data, (err) => {
-      if (err) {
-        this.emit('error', err);
-      }
-    });
+    this.blobService.createBlockBlobFromText(this.options.blobs.container, name, event.data, (err) => this.onCallback(err));
   }
 }
