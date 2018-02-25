523edd6d9be1402ef14ea5f17c8420372cc74fb9
Chris Stone <chstone@microsoft.com>
2018-02-25 05:04:49 -0500

    format log entry

--- src/bot-logger.ts
+++ src/bot-logger.ts
@@ -1,5 +1,5 @@
 import { BlobService } from 'azure-storage';
-import { Activity, Middleware, ResourceResponse } from 'botbuilder';
+import { Activity, ConversationReference, Intent, Middleware, ResourceResponse } from 'botbuilder';
 import { DocumentClient } from 'documentdb';
 import { EventEmitter } from 'events';
 import { promisify } from 'util';
@@ -25,12 +25,24 @@ export interface BotLoggerOptions extends BotLogWriterOptions {
   blobs?: BlobOptions;
 }
 
+export interface LogEntry {
+  type: string;
+  date: Date;
+  request: Partial<Activity>;
+  responses: Array<Partial<Activity>>;
+  responded: boolean;
+  conversation: Partial<ConversationReference>;
+  state: BotState;
+  topIntent: Intent;
+  error?: Error;
+}
+
 export class BotLogger implements Middleware {
   events = new EventEmitter();
   private writer: BotLogWriter;
 
   // TODO drop this after converting BotLogWriter to support Promises
-  private enqueue = promisify((obj: any, callback: Callback<any>) => this.writer.enqueue(obj, callback));
+  // private enqueue = promisify((obj: any, callback: Callback<any>) => this.writer.enqueue(obj, callback));
 
   constructor(documentClient: DocumentClient, options: BotLoggerOptions) {
     const documentWriter = new DocumentDbWriter(documentClient, options.documents);
@@ -39,8 +51,42 @@ export class BotLogger implements Middleware {
   }
 
   postActivity(context: BotContext, activities: Array<Partial<Activity>>, next: () => Promise<ResourceResponse[]>): Promise<ResourceResponse[]> {
-    console.log('are these equal?', context.responses === activities);
-    return next().then(() => this.enqueue(context));
+    const done = (err: Error) => this.onComplete(err);
+    return next().then(() => {
+      const entry = this.contextToLogEntry('postActivity', context);
+      this.writer.enqueue(entry, done);
+      return null;
+    }, (err: Error) => {
+      const entry = this.contextToLogEntry('error', context, err);
+      this.writer.enqueue(entry, done);
+    });
+  }
+
+  private onComplete(err: Error): void {
+    if (err) {
+      if (this.events.listenerCount('error') === 0) {
+        console.error('Logging error', err);
+      }
+      this.events.emit('error', err);
+    }
+  }
+
+  // TODO are there other objects that should be logged besides BotContext?
+  private contextToLogEntry(type: string, context: BotContext, error?: Error): LogEntry {
+
+    // map the relevant fields from context
+    // TODO: are there more than these?
+    return {
+      type,
+      date: new Date(),
+      request: context.request,
+      responses: context.responses,
+      responded: context.responded,
+      conversation: context.conversationReference,
+      state: context.state,
+      topIntent: context.topIntent,
+      error,
+    };
   }
 }
 
