ad2b2015e5c2c9ede6b8edb53a45a139d369de36
Chris Stone <chstone@microsoft.com>
2018-03-08 15:42:27 -0500

    add logger to context

--- src/bot-log-writer.ts
+++ src/bot-log-writer.ts
@@ -1,4 +1,8 @@
 import * as async from 'async';
+import { ConversationReference } from 'botbuilder';
+import { EventEmitter } from 'events';
+import _get = require('lodash.get');
+import _set = require('lodash.set');
 
 import { Callback } from './callback';
 import { Blob, BlobHandler, serialize } from './serialization';
@@ -18,6 +22,15 @@ export interface BlobWriter {
 export interface BotLogWriterOptions {
   /** Number of simultaneous writes before queueing (default: 1) */
   concurrency?: number;
+
+  /** Set to true to persist state object to logs (default=true) */
+  captureState?: boolean;
+
+  /** When capturing state, add properties that should NOT be stored in logs here (e.g. "user.private.password"). For supported syntax, see `lodash.get` module. */
+  privateStateProperties?: string[];
+
+  /** When masking private state variables, mask them with this string (default= character-for-character '*'s) */
+  maskPrivateStateWith?: string;
 }
 
 export interface WriteOperation {
@@ -25,7 +38,16 @@ export interface WriteOperation {
   blobs: Blob[];
 }
 
+export interface LogEntry {
+  date: Date;
+  conversation: Partial<ConversationReference>;
+  type: string;
+  data: any;
+  state: BotState;
+}
+
 export class BotLogWriter {
+  events = new EventEmitter();
   private documentQueue: async.AsyncQueue<WriteOperation>;
   private blobQueue: async.AsyncQueue<Blob>;
 
@@ -33,11 +55,25 @@ export class BotLogWriter {
     if (!options.concurrency) {
       options.concurrency = 1;
     }
+    if (this.options.captureState !== false) {
+      this.options.captureState = true;
+    }
     this.documentQueue = async.queue((op, next) => this.documentWriter.write(op, next), options.concurrency);
     this.blobQueue = async.queue((blob, next) => this.blobWriter ? this.blobWriter.write(blob, next) : next(null), options.concurrency);
   }
 
-  enqueue(entry: any, callback: Callback<any>): void {
+  write(context: BotContext, type: string, data: any): void {
+    const entry: LogEntry = {
+      date: new Date(),
+      type,
+      conversation: context.conversationReference,
+      data,
+      state: this.cleanState(context.state),
+    };
+    this.enqueue(entry, (err: Error) => this.onWrite(err));
+  }
+
+  private enqueue(entry: any, callback: Callback<any>): void {
     const blobs: Blob[] = [];
     const value = serialize(entry, (blob) => this.blobWriter.locate(blob), blobs);
 
@@ -45,5 +81,29 @@ export class BotLogWriter {
       (next: Callback<void>) => this.documentQueue.push({ value, blobs }, callback),
       (next: Callback<void>) => async.each(blobs, (blob, next) => this.blobQueue.push(blob, next), next),
     ], callback);
+  }
+
+  private onWrite(err: Error): void {
+    if (err) {
+      if (this.events.listenerCount('error') === 0) {
+        console.error('Logging error', err);
+      }
+      this.events.emit('error', err);
+    }
+  }
+
+  private cleanState(state: any): any {
+    if (this.options.captureState) {
+      if (Array.isArray(this.options.privateStateProperties)) {
+        state = JSON.parse(JSON.stringify(state)); // clone
+        this.options.privateStateProperties.forEach((x) => {
+          const redacted = _get(state, x).replace(/./g, '*');
+          _set(state, x, redacted);
+        });
+      }
+      return state;
+    } else {
+      return {};
+    }
   }
 }
--- src/bot-logger.ts
+++ src/bot-logger.ts
@@ -24,47 +24,59 @@ export interface BotLoggerOptions extends BotLogWriterOptions {
   blobs?: BlobOptions;
 }
 
-export interface LogEntry {
-  date: Date;
-  entry: Partial<Activity | ConversationReference>;
-  state: BotState;
-  error?: Error;
+export interface BotContextWithLogger extends BotContext {
+  logger: BotLogger;
 }
 
 export class BotLogger implements Middleware {
-  events = new EventEmitter();
+  events: EventEmitter;
   private writer: BotLogWriter;
 
-  constructor(documentClient: DocumentClient, options: BotLoggerOptions) {
+  constructor(private documentClient: DocumentClient, private options: BotLoggerOptions) {
     const documentWriter = new DocumentDbWriter(documentClient, options.documents);
     const blobWriter = options.blobs ? new AzureBlobWriter(options.blobs.blobService, options.blobs.options) : null;
     this.writer = new BotLogWriter(documentWriter, blobWriter, options);
+    this.events = this.writer.events;
+
+    console.log(`[BotLogger] token=${this.getId()}`);
+  }
+
+  contextCreated?(context: BotContextWithLogger, next: () => Promise<void>): Promise<void> {
+    context.logger = this;
+    return next();
   }
 
   postActivity(context: BotContext, activities: Array<Partial<Activity>>, next: () => Promise<ResourceResponse[]>): Promise<ResourceResponse[]> {
-    const done = (err: Error) => this.onComplete(err);
-    return next().then(() => {
-      [context.request].concat(context.responses)
-        .map((activity) => this.toLogEntry(activity, context.state))
-        .forEach((activity) => this.writer.enqueue(activity, done));
-      return null;
-    }, (err: Error) => {
-      this.writer.enqueue(this.toLogEntry(context.conversationReference, context.state, err), done);
-      return err;
-    });
+    return next()
+      .then((responses) => {
+        [context.request].concat(context.responses)
+          .forEach((activity) => this.write(context, activity.type, activity));
+        return responses;
+      })
+      .catch((err) => {
+        this.write(context, 'error', err);
+        throw err;
+      });
   }
 
-  private onComplete(err: Error): void {
-    if (err) {
-      if (this.events.listenerCount('error') === 0) {
-        console.error('Logging error', err);
-      }
-      this.events.emit('error', err);
-    }
+  /**
+   * Write arbitrary log event for the current conversation (the conversationReference and date are automatically logged)
+   * @param context The current context object
+   * @param type The type of log event. Use this value to filter similar events when querying the logs
+   * @param data Some arbitrary data to write
+   */
+  write(context: BotContext, type: string, data: any): void {
+    this.writer.write(context, type, data);
   }
 
-  private toLogEntry(entry: Partial<Activity | ConversationReference>, state: any, error?: Error): LogEntry {
-    return { date: new Date(), entry, state, error };
+  private getId(): string {
+    return Buffer.from(JSON.stringify({
+      type: 'cosmosdb',
+      // tslint:disable-next-line:no-string-literal
+      account: this.documentClient['sessionContainer'].getHostName(),
+      database: this.options.documents.databaseName,
+      collection: this.options.documents.collectionName,
+    })).toString('base64');
   }
 }
 
