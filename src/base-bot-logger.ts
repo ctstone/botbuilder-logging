95ee206b19624e30b45768c8e216d911f7492e96
Chris Stone <chstone@microsoft.com>
2018-06-19 09:59:37 -0400

    updates for v3

--- src/base-bot-logger.ts
+++ src/base-bot-logger.ts
@@ -1,5 +1,5 @@
 import * as async from 'async';
-import { ConversationReference, Middleware, TurnContext } from "botbuilder";
+import { IAddress, IEvent, IMiddlewareMap, Session } from "botbuilder";
 import { EventEmitter } from "events";
 import _get = require('lodash.get');
 import _set = require('lodash.set');
@@ -11,20 +11,14 @@ export const LOGGER_SERVICE_ID = Symbol('bot-logger');
 
 export interface LogEntry {
   date: Date;
-  conversation: Partial<ConversationReference>;
+  conversation: Partial<IAddress>;
   type: string;
   data: any;
-
-  // disabled: cannot access state without the original BotState middleware instance
-  // state: BotState;
 }
 
 export interface BotLogWriterOptions {
   /** Number of simultaneous writes before queueing (default: 1) */
   concurrency?: number;
-
-  /** Set to true to persist state object to logs (default=true) */
-  captureState?: boolean;
 
   /** Any properties that should NOT be visible in logs (e.g. "state.user.private.password"). For supported syntax, see `lodash.get` module. */
   maskedProperties?: string[];
@@ -48,15 +42,24 @@ export type LogWriter = (type: string, data: any) => void;
 export type DocumentWriteCallback = (err?: Error) => void;
 export type BlobWriteCallback = (err?: Error) => void;
 
-export function getLogger(context: TurnContext) {
-  return context.services.get(LOGGER_SERVICE_ID) as LogWriter;
+export function getLogger(session: Session) {
+  return session[LOGGER_SERVICE_ID] as LogWriter;
 }
 
-export function writeLog(context: TurnContext, type: string, data: any) {
-  getLogger(context)(type, data);
+function setLogger(session: Session, logger: BaseBotLogger) {
+  const logWriter: LogWriter = (type: string, data: any) => logger.write(session, type, data);
+  session[LOGGER_SERVICE_ID] = logWriter;
 }
 
-export class BaseBotLogger implements Middleware {
+function hasLogger(session: Session) {
+  return !!getLogger(session);
+}
+
+export function writeLog(session: Session, type: string, data: any) {
+  getLogger(session)(type, data);
+}
+
+export class BaseBotLogger implements IMiddlewareMap {
 
   events = new EventEmitter();
   private documentQueue: async.AsyncQueue<WriteOperation>;
@@ -66,36 +69,33 @@ export class BaseBotLogger implements Middleware {
     if (!loggerOptions.concurrency) {
       loggerOptions.concurrency = 1;
     }
-    if (this.loggerOptions.captureState !== false) {
-      this.loggerOptions.captureState = true;
-    }
     this.documentQueue = async.queue((op, next) => this.documentWriter.write(op, next), loggerOptions.concurrency);
     this.blobQueue = async.queue((blob, next) => this.blobWriter ? this.blobWriter.write(blob, next) : next(null), loggerOptions.concurrency);
   }
 
-  async onTurn(context: TurnContext, next: () => Promise<void>): Promise<void> {
-    if (!context.services.has(LOGGER_SERVICE_ID)) {
-      const logWriter: LogWriter = (type: string, data: any) => this.write(context, type, data);
-      context.services.set(LOGGER_SERVICE_ID, logWriter);
-    }
-
-    await context.onSendActivities(async (handlerContext, activities, handlerNext) => {
-      [].concat(handlerContext.activity, activities)
-        .forEach((activity) => this.write(handlerContext, activity.type, activity));
-      return await handlerNext();
-    });
-
-    await next();
+  receive(event: IEvent, next: () => void) {
+    this.write(null, event.type, event);
+    next();
   }
 
-  write(context: TurnContext, type: string, data: any): void {
+  send(event: IEvent, next: () => void) {
+    this.write(null, event.type, event);
+    next();
+  }
+
+  botbuilder(session: Session, next: () => void) {
+    if (!hasLogger(session)) {
+      setLogger(session, this);
+    }
+    next();
+  }
+
+  write(session: Session, type: string, data: any): void {
     const entry: LogEntry = {
       date: new Date(),
       type,
-      conversation: TurnContext.getConversationReference(context.activity),
+      conversation: session.message.address,
       data,
-
-      // state: this.options.captureState ? context.state : {},
     };
     this.enqueue(this.applyMask(entry), (err: Error) => this.onWrite(err));
   }
@@ -105,7 +105,7 @@ export class BaseBotLogger implements Middleware {
     const value = serialize(entry, (blob) => this.blobWriter.locate(blob), blobs);
 
     async.parallel([
-      (next: Callback<void>) => this.documentQueue.push({ value, blobs }, callback),
+      (next: Callback<void>) => this.documentQueue.push({ value, blobs }, next),
       (next: Callback<void>) => async.each(blobs, (blob, next) => this.blobQueue.push(blob, next), next),
     ], callback);
   }
